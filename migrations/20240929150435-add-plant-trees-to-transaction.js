e8bffc4b079afb913a28e5f4958cdbe8fe2b3c7b
Dharshini <dharshinipandian2005@gmail.com>
2024-09-29 21:38:12 +0530

    post api was written and completion of swagger documentation

---
+++ migrations/20240929150435-add-plant-trees-to-transaction.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('transactions', 'plant_trees', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('transactions', 'plant_trees');
+  }
+};
